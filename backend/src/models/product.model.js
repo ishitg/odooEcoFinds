ea8c0350aecf6135794c59e2cdebc5169d1da157
AjinkyaJoshi05 <aaj.ajinkya@gmail.com>
2025-09-06 13:32:19 +0530

    data models

--- backend/src/models/product.model.js
+++ backend/src/models/product.model.js
@@ -1,1 +1,40 @@
+import mongoose from "mongoose";
 
+const productSchema = new mongoose.Schema({
+  description:{
+    required: true,
+    type: String,
+  },
+  title:{
+    required: true,
+    type: String,
+  },
+  productImage:[{
+    type:String,
+    required:true,
+  }],
+  price:{
+    type: Number,
+    default:0,
+    required:true,
+  },
+  stock:{
+    default:0,
+    type: Number,
+  },
+  catgeory:{
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Category",
+    required: true,
+  },
+  owner:{
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+  },
+  status: String,
+  enum: ["SOLD","UNSOLD"],
+  defult: "UNSOLD",
+
+},{timestamps:true});
+
+export const Product = mongoose.model("Product",productSchema);
