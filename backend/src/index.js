82f3cc5a331601a9861a7a5d89a992d081d0d9d7
AjinkyaJoshi05 <aaj.ajinkya@gmail.com>
2025-09-06 11:00:36 +0530

    Backend Repository setup

---
+++ backend/src/index.js
@@ -0,0 +1,16 @@
+import dotenv from "dotenv";
+import {app} from "./app.js"
+dotenv.config({
+    path: './.env'
+})
+
+import connectDB from "./db/index.js";
+connectDB()
+.then(()=>{
+    app.listen(process.env.PORT || 8000, () =>{
+        console.log(`Server is running on port : ${process.env.PORT}`);
+    })
+})
+.catch((err)=>{
+    console.log("Mongo DB connection failed !!! ",err);
+})
